dab6133eda4899abb647ae6aeb9c965e93453eb2
mmeest <martin.maasik.001@mail.ee>
2025-04-22 21:30:32 +0300

    new

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/milrem_berserker/', 
+  base: '/Milrem_Berserker/', 
   plugins: [vue()],
 })
